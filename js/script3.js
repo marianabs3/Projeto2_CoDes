b26adddc5863220f84493d8570a34eb21a5cd86c
ivanabb2 <62958684+ivanabb2@users.noreply.github.com>
2020-12-01 19:02:11 -0300

    creusa

---
+++ js/script3.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    abrir = document.querySelector('.abrir')
+
+    janela = document.querySelector('.janela')
+
+    fechar = document.querySelector('.fechar')
+
+    abrir.addEventListener('click', function(event) {
+        janela.style.display = 'block'
+    })
+
+    fechar.addEventListener('click', function(event) {
+        janela.style.display = 'none'
+    })
+
+})
